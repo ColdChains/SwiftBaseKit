beed68604e6ec4e3a6749ab9d2bb8364cea8429d
ColdChains <coldchains@qq.com>
2022-09-28 16:13:28 +0800

    0.1.0

---
+++ SwiftBaseKit/SwiftBaseKit.h
@@ -0,0 +1,18 @@
+//
+//  SwiftBaseKit.h
+//  SwiftBaseKit
+//
+//  Created by lax on 2022/9/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftBaseKit.
+FOUNDATION_EXPORT double SwiftBaseKitVersionNumber;
+
+//! Project version string for SwiftBaseKit.
+FOUNDATION_EXPORT const unsigned char SwiftBaseKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftBaseKit/PublicHeader.h>
+
+
---
+++ SwiftBaseKitDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwiftBaseKitDemo
+//
+//  Created by lax on 2022/9/28.
+//
+
+import UIKit
+import SwiftBaseKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
